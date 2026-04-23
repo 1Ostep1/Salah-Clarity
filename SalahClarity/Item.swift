469928a3c1870d91105bcf8414ac7a3f200a84f2
Ramazan Iusupov <iusupov.ramazan.i@gmail.com>
2026-04-24 00:04:11 +0600

    Initial Commit

---
+++ SalahClarity/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  SalahClarity
+//
+//  Created by Ramazan Iusupov on 24/4/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ SalahClarity/SalahClarityApp.swift
@@ -0,0 +1,32 @@
+//
+//  SalahClarityApp.swift
+//  SalahClarity
+//
+//  Created by Ramazan Iusupov on 24/4/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct SalahClarityApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
